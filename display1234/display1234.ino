da58575c4f06e534f2f8c26960e754d87ab21968
davide <toscaninidavide.istruzione@gmail.com>
2018-05-07 08:27:11 +0200

    codice display ?

---
+++ display1234/display1234.ino
@@ -0,0 +1,49 @@
+/*
+  LiquidCrystal Library - Hello World
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+#define int PinVal 5;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  pinMode(pinVal, INPUT);
+  /* Print a message to the LCD.
+  lcd.print("hello, world!");
+  */
+}
+
+void leggiVal(String val){
+    lcd.print(val);
+ }
+
+void loop() {
+  // set the cursor to column 0, line 1 (second)
+  lcd.setCursor(0, 1);
+
+   veggiVal(PinVal);
+  
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
+
